57d9a78028b5dd416d48009e6ddf256bccd1d786
TaewoongR <iwoong98@kau.kr>
2025-02-10 23:08:56 +0900

    refactor/#10 : Image Caption 구현체, API 인터페이스, API 모델 선언

---
+++ data/ai/src/main/java/com/boostcamp/ai/imagecaption/ComputerVisionApi.kt
@@ -0,0 +1,19 @@
+package com.boostcamp.ai.imagecaption
+
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface ComputerVisionApi {
+	@POST("computervision/imageanalysis:analyze")
+	suspend fun analyzeImage(
+		@Header("Ocp-Apim-Subscription-Key") subscriptionKey: String,
+		@Header("Content-Type") contentType: String = "application/octet-stream",
+		@Query("api-version") apiVersion: String = "2024-02-01",
+		@Query("features") features: String = "denseCaptions",
+		@Query("language") language: String = "en",
+		@Body request: RequestBody
+	): ImageAnalysisResponse
+}
---
+++ data/ai/src/main/java/com/boostcamp/ai/imagecaption/ImageAnalysisResponse.kt
@@ -0,0 +1,37 @@
+package com.boostcamp.ai.imagecaption
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ImageAnalysisResponse(
+	@SerialName("denseCaptionsResult") val denseCaptionsResult: DenseCaptionsResult,
+	@SerialName("metadata") val metadata: Metadata,
+	@SerialName("modelVersion") val modelVersion: String
+)
+
+@Serializable
+data class DenseCaptionsResult(
+	@SerialName("values") val values: List<Value>
+)
+
+@Serializable
+data class Value(
+	@SerialName("boundingBox") val boundingBox: BoundingBox,
+	@SerialName("confidence") val confidence: Double,
+	@SerialName("text") val text: String
+)
+
+@Serializable
+data class BoundingBox(
+	@SerialName("h") val h: Int,
+	@SerialName("w") val w: Int,
+	@SerialName("x") val x: Int,
+	@SerialName("y") val y: Int
+)
+
+@Serializable
+data class Metadata(
+	@SerialName("height") val height: Int,
+	@SerialName("width") val width: Int
+)
