38fd74b15bfd993aa939e82e1b21e1eee1c3dcab
TaewoongR <iwoong98@kau.kr>
2025-02-10 23:11:43 +0900

    refactor/#10 : MultiModal UseCase DI 모듈 선언

---
+++ feature/episode/src/main/java/com/boostcamp/mapisode/episode/di/ProvideAiUseCase.kt
@@ -0,0 +1,29 @@
+package com.boostcamp.mapisode.episode.di
+
+import com.boostcamp.ai.EpisodeGenerationUseCase
+import com.boostcamp.ai.ImageCaptionRepository
+import com.boostcamp.ai.LlmRepository
+import com.boostcamp.ai.TranslationRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+	@Provides
+	@Singleton
+	fun provideAiUseCase(
+		imageCaptionRepository: ImageCaptionRepository,
+		llmRepository: LlmRepository,
+		translationRepository: TranslationRepository,
+    ): EpisodeGenerationUseCase {
+		return EpisodeGenerationUseCase(
+            imageCaptionRepository,
+            llmRepository,
+            translationRepository,
+        )
+	}
+}
