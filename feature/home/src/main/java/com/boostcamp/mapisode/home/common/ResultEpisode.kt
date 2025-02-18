6306906bd5b4f48d31c178311cbe221f8c2408e7
shinythinking <shinythink@gmail.com>
2025-02-18 13:50:21 +0900

    feat/#17 : Result 타입 설정

---
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/common/ResultEpisode.kt
@@ -0,0 +1,12 @@
+package com.boostcamp.mapisode.home.common
+
+import com.naver.maps.geometry.LatLng
+
+data class ResultEpisode(
+	val id: String,
+	val owner: String,
+	val distance: String,
+	val reason: String,
+	val thumbnail: String,
+	val coordinates: LatLng
+)
