a530110c013cbba51c39fa59a9c6bfe5636fced5
shinythinking <shinythink@gmail.com>
2025-02-18 17:49:50 +0900

    fix/#17 : ktlint 대응

--- feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationSideEffect.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationSideEffect.kt
@@ -2,5 +2,4 @@ package com.boostcamp.mapisode.home.ai
 
 import com.boostcamp.mapisode.ui.base.SideEffect
 
-sealed class RecommendationSideEffect : SideEffect {
-}
+sealed class RecommendationSideEffect : SideEffect
