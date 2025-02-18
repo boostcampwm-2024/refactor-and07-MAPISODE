3a6b629e95005fcf1aa2217f3849010dd9bb41e0
shinythinking <shinythink@gmail.com>
2025-02-18 17:06:04 +0900

    feat/#17 : viewModel과 Screen 연결

--- feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationIntent.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationIntent.kt
@@ -1,6 +1,10 @@
 package com.boostcamp.mapisode.home.ai
 
+import com.boostcamp.mapisode.home.common.OptionType
 import com.boostcamp.mapisode.ui.base.UiIntent
 
 sealed class RecommendationIntent : UiIntent {
+	data class OptionClick(val optionId: OptionType) : RecommendationIntent()
+	data object ShowMapType : RecommendationIntent()
+	data object ShowListType : RecommendationIntent()
 }
--- feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationState.kt
+++ feature/home/src/main/java/com/boostcamp/mapisode/home/ai/RecommendationState.kt
@@ -1,7 +1,14 @@
 package com.boostcamp.mapisode.home.ai
 
+import com.boostcamp.mapisode.home.common.OptionType
+import com.boostcamp.mapisode.home.common.ResultEpisode
+import com.boostcamp.mapisode.home.common.ResultViewType
 import com.boostcamp.mapisode.ui.base.UiState
 
 data class RecommendationState (
-	val a:Int = 0
+	val type: OptionType = OptionType.NONE,
+	val isOptionSelected: Boolean = false,
+	val resultViewType: ResultViewType = ResultViewType.LIST_VIEW,
+	val resultEpisodes: List<ResultEpisode> = emptyList(),
+
 ) : UiState
