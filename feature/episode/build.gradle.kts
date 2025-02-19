554b573cf43b45e681eaf44a9347f41275ebe73d
TaewoongR <iwoong98@kau.kr>
2025-02-20 04:57:58 +0900

    refactor/#20 : tensorflow lite 라이브러리 및 모델 적용

--- feature/episode/build.gradle.kts
+++ feature/episode/build.gradle.kts
@@ -15,10 +15,15 @@ android {
 			throw GradleException("GOOGLE_GENERATIVE_AI Key is not set.")
 		}
 		buildConfigField("String", "GOOGLE_GENERATIVE_AI", "\"$googleGenerativeAi\"")
+
+		ndk {
+			abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
+		}
 	}
 
 	buildFeatures {
 		buildConfig = true
+		mlModelBinding = true
 	}
 }
 
@@ -26,7 +31,10 @@ dependencies {
 	implementation(project.libs.bundles.naverMap)
 	implementation(project.libs.bundles.coil)
 	implementation(project.libs.google.cloud.generativeai)
-	implementation(project.libs.litert)
+
+	implementation(libs.tensorflow.lite.support)
+	implementation(libs.tensorflow.lite.metadata)
+	implementation(libs.tensorflow.lite.gpu)
 
 	implementation(projects.core.network)
 	implementation(projects.domain.episode)
